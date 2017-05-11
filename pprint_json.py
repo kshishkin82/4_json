37c681a0f353228f1d09887a894c2e367a764b27
Konstantin <k.shishkin@brandquad.ru>
2017-05-11 10:25:49 +0000

    Fix

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,29 @@
+import sys
+import os
 import json
 
 
 def load_data(filepath):
-    pass
-
+    with open(filepath, 'r') as f:
+        # print (f.read())
+        data = json.loads(f.read())
+    return data
 
 def pretty_print_json(data):
-    pass
+    print (json.dumps(data, sort_keys=True, indent=4, separators=(',', ': ')))
 
 
 if __name__ == '__main__':
-    pass
+
+    if len(sys.argv) == 1:
+        print ("Файл не передан")
+        sys.exit()
+        
+    f = sys.argv[1]
+    if os.path.exists(f) is False:
+        print ("Файл не найден")
+        sys.exit()
+        
+    data = load_data(f)
+    pretty_print_json(data)
+    
